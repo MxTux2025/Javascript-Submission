9382551aa5a0629e551d67f8a404b5ce4059815f
Jm <206396009+MxTux2025@users.noreply.github.com>
2026-05-21 21:54:24 +0800

    Add files via upload
    Soal dan Latihan Function Javascript

---
+++ evaluasi-2.js
@@ -0,0 +1,39 @@
+// Soal 1 Basic Function: Lampu menyala
+
+/*
+function lampu(kalimenyala) {
+    const menit = 60;
+    let menyalaTiap = 5; 
+
+    let detikYangDibutuhkan = kalimenyala * menyalaTiap;
+    let waktu = detikYangDibutuhkan / menit;
+    
+    return `Dibutuhkan waktu ${waktu} menit untuk menyala sebanyak ${kalimenyala}` 
+};
+*/
+
+// Soal 2 Hitung Umur
+
+/*
+function AgeCalc(tahunLahir, tahunSekarang) {
+    let umur = tahunSekarang - tahunLahir;
+    return `saya lahir tahun ${tahunLahir}, karena sekarang tahun ${tahunSekarang} maka umur saya ${umur} tahun`
+};
+*/
+
+// Soal 3 Perkenalan
+/*
+const hello = (name, umur = 17, origin = "Bandung") => {
+    return `Perkenalkan Nama saya ${name}, umur saya ${umur}, Saya tinggal di ${origin}.`
+};
+*/
+
+
+// Soal 4 Luas Persegi 
+/*
+function Persegi(panjang, lebar) {
+    let luasPersegiPanjang = panjang * lebar;
+    return luasPersegiPanjang
+};
+*/
+
